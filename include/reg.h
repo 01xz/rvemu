bd9c9de0312a7cbc6e6fbf8594fa2df7e1cd54f5
01xz <73350430+01xz@users.noreply.github.com>
2023-05-06 20:42:29 +0800

    style: rename to `kGpRegNum`

--- include/reg.h
+++ include/reg.h
@@ -9,7 +9,7 @@ typedef enum {
   kA0, kA1, kA2, kA3, kA4, kA5, kA6, kA7,
   kS2, kS3, kS4, kS5, kS6, kS7, kS8, kS9, kS10, kS11,
   kT3, kT4, kT5, kT6,
-  kNumGpReg,
+  kGpRegNum,
 } GpRegType;
 
 #endif  // RVEMU_REG_H_
