a57d83a25e79b408bff52d2b7ce15217bca0ec60
01xz <73350430+01xz@users.noreply.github.com>
2023-05-10 15:27:27 +0800

    fix: set `pc` with `re_enter_pc`

--- src/machine.c
+++ src/machine.c
@@ -8,13 +8,19 @@
 
 ExitReason machine_step(Machine* m) {
   while (true) {
+    m->state.exit_reason = kNone;
     exec_block_interp(&m->state);
+
+    assert(m->state.exit_reason != kNone);
     if (m->state.exit_reason == kDirectBranch ||
         m->state.exit_reason == kIndirectBranch) {
+      m->state.pc = m->state.re_enter_pc;
       continue;  // JIT
     }
     break;
   }
+
+  m->state.pc = m->state.re_enter_pc;
   assert(m->state.exit_reason == kECall);
   return kECall;
 }
