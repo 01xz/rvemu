fba2c35e894742e73aa8c1cf3a853d95a229f6c2
01xz <73350430+01xz@users.noreply.github.com>
2023-05-10 15:21:35 +0800

    feat: type for F, D, and Zicsr extension

--- include/reg.h
+++ include/reg.h
@@ -1,6 +1,8 @@
 #ifndef RVEMU_REG_H_
 #define RVEMU_REG_H_
 
+#include "types.h"
+
 typedef enum {
   kZero,
   kRa, kSp, kGp, kTp,
@@ -11,5 +13,27 @@ typedef enum {
   kT3, kT4, kT5, kT6,
   kGpRegNum,
 } GpRegType;
+
+typedef enum {
+  kFt0, kFt1, kFt2, kFt3, kFt4 ,kFt5, kFt6, kFt7,
+  kFs0, kFs1,
+  kFa0, kFa1, kFa2, kFa3, kFa4, kFa5, kFa6, kFa7,
+  kFs2, kFs3, kFs4, kFs5, kFs6, kFs7, kFs8, kFs9, kFs10, kFs11,
+  kFt8, kFt9, kFt10, kFt11,
+  kFpRegNum,
+} FpRegType;
+
+typedef union {
+  u64 w64;
+  u32 w32;
+  f64 d;
+  f32 s;
+} FpReg;
+
+typedef enum {
+  kFflags = 0x001,
+  kFrm    = 0x002,
+  kFcsr   = 0x003,
+} Csr;
 
 #endif  // RVEMU_REG_H_
