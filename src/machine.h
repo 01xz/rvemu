d6340a84116e5015c0513054de4d1615d1579710
01xz <73350430+01xz@users.noreply.github.com>
2023-09-06 20:44:29 +0800

    feat: get and set RegX

--- src/machine.h
+++ src/machine.h
@@ -17,4 +17,14 @@ void machine_setup(Machine*, int, char**);
 
 ExitReason machine_step(Machine*);
 
+inline u64 machine_get_regx(Machine* m, int reg) {
+  assert(reg > 0 && reg <= kGpRegNum);
+  return m->state.gp_regs[reg];
+}
+
+inline void machine_set_regx(Machine* m, int reg, u64 reg_val) {
+  assert(reg > 0 && reg <= kGpRegNum);
+  m->state.gp_regs[reg] = reg_val;
+}
+
 #endif  // RVEMU_MACHINE_H_
