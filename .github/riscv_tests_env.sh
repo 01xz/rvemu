17334ab91f6037f1e037306f7a2af2e5e2009223
01xz <73350430+01xz@users.noreply.github.com>
2023-11-22 22:46:59 +0800

    ci: prepare and run riscv-tests

---
+++ .github/riscv_tests_env.sh
@@ -0,0 +1,19 @@
+export PATH=$RISCV/bin:$PATH
+
+TARGET=riscv64-unknown-elf
+
+mkdir -p $RISCV
+
+curl -Lo /tmp/riscv.tar.gz https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2023.11.22/riscv64-elf-ubuntu-22.04-gcc-nightly-2023.11.22-nightly.tar.gz
+
+tar -xvf /tmp/riscv.tar.gz -C $RISCV --strip-components 1
+
+git clone https://github.com/riscv-software-src/riscv-tests.git /tmp/riscv-tests
+
+cd /tmp/riscv-tests
+git submodule update --init --recursive
+autoconf
+autoupdate
+./configure --prefix=$RISCV/$TARGET
+make -j `nproc`
+make install
