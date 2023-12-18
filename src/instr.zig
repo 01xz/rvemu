678576075da5e87d947458a5d94db456151edaa1
01xz <73350430+01xz@users.noreply.github.com>
2023-12-18 21:49:42 +0800

    fix: decode atomic and floating-point instructions

--- src/instr.zig
+++ src/instr.zig
@@ -43,6 +43,14 @@ pub const Instr = union(enum) {
             regw = 0b0_1110,
             fence = 0b0_0011,
             system = 0b1_1100,
+            atomic = 0b0_1011,
+            load_fp = 0b0_0001,
+            store_fp = 0b0_1001,
+            fmadd = 0b1_0000,
+            fmsub = 0b1_0001,
+            fnmsub = 0b1_0010,
+            fnmadd = 0b1_0011,
+            fp = 0b1_0100,
             _,
         };
 
@@ -74,6 +82,22 @@ pub const Instr = union(enum) {
             .fence => decodeFence(raw),
 
             .system => decodeSystem(raw),
+
+            .atomic => decodeAtomic(raw),
+
+            .load_fp => decodeLoadFp(raw),
+
+            .store_fp => decodeStoreFp(raw),
+
+            .fmadd => decodeFmadd(raw),
+
+            .fmsub => decodeFmadd(raw),
+
+            .fnmsub => decodeFmadd(raw),
+
+            .fnmadd => decodeFmadd(raw),
+
+            .fp => decodeFp(raw),
 
             else => null,
         };
@@ -147,6 +171,90 @@ pub const Instr = union(enum) {
             system = .{ .zicsr = Zicsr.decodeCsr(raw) };
         }
         return system;
+    }
+
+    inline fn decodeAtomic(raw: u32) Self {
+        var atomic: Self = undefined;
+        if (RV32A.decodeAtomic(raw)) |rv32a| {
+            atomic = .{ .rv32a = rv32a };
+        } else {
+            atomic = .{ .rv64a = RV64A.decodeAtomic(raw) };
+        }
+        return atomic;
+    }
+
+    inline fn decodeLoadFp(raw: u32) Self {
+        var load_fp: Self = undefined;
+        if (RV32F.decodeLoadFp(raw)) |rv32f| {
+            load_fp = .{ .rv32f = rv32f };
+        } else {
+            load_fp = .{ .rv64f = RV64F.decodeLoadFp(raw) };
+        }
+        return load_fp;
+    }
+
+    inline fn decodeStoreFp(raw: u32) Self {
+        var store_fp: Self = undefined;
+        if (RV32F.decodeStoreFp(raw)) |rv32f| {
+            store_fp = .{ .rv32f = rv32f };
+        } else {
+            store_fp = .{ .rv64f = RV64F.decodeStoreFp(raw) };
+        }
+        return store_fp;
+    }
+
+    inline fn decodeFmadd(raw: u32) Self {
+        var fmadd: Self = undefined;
+        if (RV32F.decodeFmadd(raw)) |rv32f| {
+            fmadd = .{ .rv32f = rv32f };
+        } else {
+            fmadd = .{ .rv64f = RV64F.decodeFmadd(raw) };
+        }
+        return fmadd;
+    }
+
+    inline fn decodeFmsub(raw: u32) Self {
+        var fmsub: Self = undefined;
+        if (RV32F.decodeFmsub(raw)) |rv32f| {
+            fmsub = .{ .rv32f = rv32f };
+        } else {
+            fmsub = .{ .rv64f = RV64F.decodeFmsub(raw) };
+        }
+        return fmsub;
+    }
+
+    inline fn decodeFnmsub(raw: u32) Self {
+        var fnmsub: Self = undefined;
+        if (RV32F.decodeFnmsub(raw)) |rv32f| {
+            fnmsub = .{ .rv32f = rv32f };
+        } else {
+            fnmsub = .{ .rv64f = RV64F.decodeFnmsub(raw) };
+        }
+        return fnmsub;
+    }
+
+    inline fn decodeFnmadd(raw: u32) Self {
+        var fnmadd: Self = undefined;
+        if (RV32F.decodeFnmadd(raw)) |rv32f| {
+            fnmadd = .{ .rv32f = rv32f };
+        } else {
+            fnmadd = .{ .rv64f = RV64F.decodeFnmadd(raw) };
+        }
+        return fnmadd;
+    }
+
+    inline fn decodeFp(raw: u32) Self {
+        var fp: Self = undefined;
+        if (RV32F.decodeFp(raw)) |rv32f| {
+            fp = .{ .rv32f = rv32f };
+        } else if (RV64F.decodeFp(raw)) |rv64f| {
+            fp = .{ .rv64f = rv64f };
+        } else if (RV64D.decodeFp(raw)) |rv32d| {
+            fp = .{ .rv32d = rv32d };
+        } else {
+            fp = .{ .rv64d = RV64D.decodeFp(raw) };
+        }
+        return fp;
     }
 
     inline fn decodeRvc(raw: u16) Self {
@@ -473,9 +581,25 @@ const RV32A = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeAtomic(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        const funct5: u5 = @intCast(raw >> 27 & 0b1_1111);
+        return if (funct3 == 0b010)
+            switch (funct5) {
+                0b0_0010 => .{ .lr_w = RType.decode(raw) },
+                0b0_0011 => .{ .sc_w = RType.decode(raw) },
+                0b0_0001 => .{ .amoswap_w = RType.decode(raw) },
+                0b0_0000 => .{ .amoadd_w = RType.decode(raw) },
+                0b0_0100 => .{ .amoxor_w = RType.decode(raw) },
+                0b0_1100 => .{ .amoand_w = RType.decode(raw) },
+                0b0_1000 => .{ .amoor_w = RType.decode(raw) },
+                0b1_0000 => .{ .amomin_w = RType.decode(raw) },
+                0b1_0100 => .{ .amomax_w = RType.decode(raw) },
+                0b1_1000 => .{ .amominu_w = RType.decode(raw) },
+                0b1_1100 => .{ .amomaxu_w = RType.decode(raw) },
+            }
+        else
+            null;
     }
 };
 
@@ -494,9 +618,25 @@ const RV64A = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeAtomic(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        const funct5: u5 = @intCast(raw >> 27 & 0b1_1111);
+        return if (funct3 == 0b011)
+            switch (funct5) {
+                0b0_0010 => .{ .lr_d = RType.decode(raw) },
+                0b0_0011 => .{ .sc_d = RType.decode(raw) },
+                0b0_0001 => .{ .amoswap_d = RType.decode(raw) },
+                0b0_0000 => .{ .amoadd_d = RType.decode(raw) },
+                0b0_0100 => .{ .amoxor_d = RType.decode(raw) },
+                0b0_1100 => .{ .amoand_d = RType.decode(raw) },
+                0b0_1000 => .{ .amoor_d = RType.decode(raw) },
+                0b1_0000 => .{ .amomin_d = RType.decode(raw) },
+                0b1_0100 => .{ .amomax_d = RType.decode(raw) },
+                0b1_1000 => .{ .amominu_d = RType.decode(raw) },
+                0b1_1100 => .{ .amomaxu_d = RType.decode(raw) },
+            }
+        else
+            null;
     }
 };
 
@@ -530,9 +670,122 @@ const RV32F = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeLoadFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        return if (funct3 == 0b010)
+            .{ .flw = IType.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeStoreFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        return if (funct3 == 0b010)
+            .{ .fsw = SType.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFmadd(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b00)
+            .{ .fmadd_s = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFmsub(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b00)
+            .{ .fmsub_s = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFnmsub(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b00)
+            .{ .fnmsub_s = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFnmadd(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b00)
+            .{ .fnmadd_s = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        const rs2: u5 = @intCast((raw >> 20) & 0b1_1111);
+        const funct7: u7 = @intCast(raw >> 25 & 0b111_1111);
+        return switch (funct7) {
+            0b000_0000 => .{ .fadd_s = RType.decode(raw) },
+            0b000_0100 => .{ .fsub_s = RType.decode(raw) },
+            0b000_1000 => .{ .fmul_s = RType.decode(raw) },
+            0b000_1100 => .{ .fdiv_s = RType.decode(raw) },
+
+            0b010_1100 => if (rs2 == 0b0_0000)
+                .{ .fsqrt_s = RType.decode(raw) }
+            else
+                null,
+
+            0b001_0000 => if (funct3 == 0b000)
+                .{ .fsgnj_s = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .fsgnjn_s = RType.decode(raw) }
+            else if (funct3 == 0b010)
+                .{ .fsgnjx_s = RType.decode(raw) }
+            else
+                null,
+
+            0b001_0100 => if (funct3 == 0b000)
+                .{ .fmin_s = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .fmax_s = RType.decode(raw) }
+            else
+                null,
+
+            0b110_0000 => if (rs2 == 0b0_0000)
+                .{ .fcvt_w_s = RType.decode(raw) }
+            else if (rs2 == 0b0_0001)
+                .{ .fcvt_wu_s = RType.decode(raw) }
+            else
+                null,
+
+            0b111_0000 => if (rs2 == 0b0_0000 and funct3 == 0b000)
+                .{ .fmv_x_w = RType.decode(raw) }
+            else if (rs2 == 0b0_0000 and funct3 == 0b001)
+                .{ .fclass_s = RType.decode(raw) }
+            else
+                null,
+
+            0b101_0000 => if (funct3 == 0b010)
+                .{ .feq_s = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .flt_s = RType.decode(raw) }
+            else if (funct3 == 0b000)
+                .{ .fle_s = RType.decode(raw) }
+            else
+                null,
+
+            0b110_1000 => if (rs2 == 0b0_0000)
+                .{ .fcvt_s_w = RType.decode(raw) }
+            else if (rs2 == 0b0_0001)
+                .{ .fcvt_s_wu = RType.decode(raw) }
+            else
+                null,
+
+            0b111_1000 => if (rs2 == 0b0_0000 and funct3 == 0b000)
+                .{ .fmv_w_x = RType.decode(raw) }
+            else
+                null,
+
+            else => null,
+        };
     }
 };
 
@@ -544,9 +797,16 @@ const RV64F = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeFp(raw: u32) ?Self {
+        const rs2: u5 = @intCast((raw >> 20) & 0b1_1111);
+        const funct7: u7 = @intCast(raw >> 25 & 0b111_1111);
+        return switch (tuple(funct7, rs2)) {
+            tuple(0b110_0000, 0b0_0010) => .{ .fcvt_l_s = RType.decode(raw) },
+            tuple(0b110_0000, 0b0_0011) => .{ .fcvt_lu_s = RType.decode(raw) },
+            tuple(0b110_1000, 0b0_0010) => .{ .fcvt_s_l = RType.decode(raw) },
+            tuple(0b110_1000, 0b0_0011) => .{ .fcvt_s_lu = RType.decode(raw) },
+            else => null,
+        };
     }
 };
 
@@ -580,9 +840,125 @@ const RV32D = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeLoadFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        return if (funct3 == 0b011)
+            .{ .fld = IType.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeStoreFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        return if (funct3 == 0b011)
+            .{ .fsd = SType.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFmadd(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b01)
+            .{ .fmadd_d = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFmsub(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b01)
+            .{ .fmsub_d = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFnmsub(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b01)
+            .{ .fnmsub_d = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFnmadd(raw: u32) ?Self {
+        const funct2: u2 = @intCast((raw >> 25) & 0b11);
+        return if (funct2 == 0b01)
+            .{ .fnmadd_d = R4Type.decode(raw) }
+        else
+            null;
+    }
+
+    pub inline fn decodeFp(raw: u32) ?Self {
+        const funct3: u3 = @intCast((raw >> 12) & 0b111);
+        const rs2: u5 = @intCast((raw >> 20) & 0b1_1111);
+        const funct7: u7 = @intCast(raw >> 25 & 0b111_1111);
+        return switch (funct7) {
+            0b000_0001 => .{ .fadd_d = RType.decode(raw) },
+            0b000_0101 => .{ .fsub_d = RType.decode(raw) },
+            0b000_1001 => .{ .fmul_d = RType.decode(raw) },
+            0b000_1101 => .{ .fdiv_d = RType.decode(raw) },
+
+            0b010_1101 => if (rs2 == 0b0_0000)
+                .{ .fsqrt_d = RType.decode(raw) }
+            else
+                null,
+
+            0b001_0001 => if (funct3 == 0b000)
+                .{ .fsgnj_d = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .fsgnjn_d = RType.decode(raw) }
+            else if (funct3 == 0b010)
+                .{ .fsgnjx_d = RType.decode(raw) }
+            else
+                null,
+
+            0b001_0101 => if (funct3 == 0b000)
+                .{ .fmin_s = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .fmax_s = RType.decode(raw) }
+            else
+                null,
+
+            0b010_0000 => if (rs2 == 0b0_0001)
+                .{ .fcvt_s_d = RType.decode(raw) }
+            else
+                null,
+
+            0b010_0001 => if (rs2 == 0b0_0000)
+                .{ .fcvt_d_s = RType.decode(raw) }
+            else
+                null,
+
+            0b101_0001 => if (funct3 == 0b010)
+                .{ .feq_d = RType.decode(raw) }
+            else if (funct3 == 0b001)
+                .{ .flt_d = RType.decode(raw) }
+            else if (funct3 == 0b000)
+                .{ .fle_d = RType.decode(raw) }
+            else
+                null,
+
+            0b111_0001 => if (rs2 == 0b0_0000 and funct3 == 0b001)
+                .{ .fclass_d = RType.decode(raw) }
+            else
+                null,
+
+            0b110_0001 => if (rs2 == 0b0_0000)
+                .{ .fcvt_w_d = RType.decode(raw) }
+            else if (rs2 == 0b0_0001)
+                .{ .fcvt_wu_d = RType.decode(raw) }
+            else
+                null,
+
+            0b111_1001 => if (rs2 == 0b0_0000)
+                .{ .fcvt_d_w = RType.decode(raw) }
+            else if (rs2 == 0b0_0001)
+                .{ .fcvt_d_wu = RType.decode(raw) }
+            else
+                null,
+
+            else => null,
+        };
     }
 };
 
@@ -596,9 +972,18 @@ const RV64D = union(enum) {
 
     const Self = @This();
 
-    pub inline fn decode(raw: u32) ?Self {
-        _ = raw;
-        return .{};
+    pub inline fn decodeFp(raw: u32) ?Self {
+        const rs2: u5 = @intCast((raw >> 20) & 0b1_1111);
+        const funct7: u7 = @intCast(raw >> 25 & 0b111_1111);
+        return switch (tuple(funct7, rs2)) {
+            tuple(0b110_0001, 0b0_0010) => .{ .fcvt_l_d = RType.decode(raw) },
+            tuple(0b110_0001, 0b0_0011) => .{ .fcvt_lu_d = RType.decode(raw) },
+            tuple(0b111_0001, 0b0_0000) => .{ .fmv_x_d = RType.decode(raw) },
+            tuple(0b110_1001, 0b0_0010) => .{ .fcvt_d_l = RType.decode(raw) },
+            tuple(0b110_1001, 0b0_0011) => .{ .fcvt_d_lu = RType.decode(raw) },
+            tuple(0b111_1001, 0b0_0000) => .{ .fmv_d_x = RType.decode(raw) },
+            else => null,
+        };
     }
 };
 
